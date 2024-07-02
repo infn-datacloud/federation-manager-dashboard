66890403f8964f62d2d5fe5dc1a94743461c90f0
ettore-infn <ettore.serra@ba.infn.it>
2024-07-02 16:28:49 +0200

    added loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { Box, Typography } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#162D4D',
+		},
+	},
+});
+
+export default function Loading() {
+	return (
+		<Box
+			width='100%'
+			height='80vh'
+			display='flex'
+			flexDirection='column'
+			alignItems='center'
+			justifyContent='center'
+		>
+			<br />
+			<br />
+			<br />
+			<ThemeProvider theme={theme}>
+				<CircularProgress color='primary' />
+			</ThemeProvider>
+			<br />
+			<Typography variant='h6'>Loading...</Typography>
+		</Box>
+	);
+}
