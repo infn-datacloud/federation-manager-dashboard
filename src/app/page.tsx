489e277788be2cc1588350781f6b597f88389fbd
ettore-infn <ettore.serra@ba.infn.it>
2025-08-28 16:34:15 +0200

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,39 @@
+import Header from '@/components/header';
+import Box from '@/components/box';
+import { CloudIcon, IdentificationIcon, DocumentTextIcon } from '@heroicons/react/16/solid';
+
 export default function Home() {
-	  return <div>Welcome to Federation Manager Dashboard</div>;
+	return (
+		<>
+			<Header
+				logo='/logos/infn_logo.png'
+				title='Federation Manager'
+				subtitle='Federation Manager: seamlessly integrating providers and communities into DataCloud with simplicity, security, and automated resource management.'
+			/>
+			<Box
+				title='Providers'
+				subtitle='Short definition of Providers'
+				type='small'
+				btnText='Show All'
+				btnHref='/providers'
+				icon={<CloudIcon />}
+			/>
+			<Box
+				title='Identity Providers'
+				subtitle='Short definition of Identity Providers'
+				type='small'
+				btnText='Show All'
+				btnHref='/idps'
+				icon={<IdentificationIcon />}
+			/>
+			<Box
+				title='SLAs'
+				subtitle='Short definition of SLAs'
+				type='small'
+				btnText='Show All'
+				btnHref='/slas'
+				icon={<DocumentTextIcon />}
+			/>
+		</>
+	);
 }
