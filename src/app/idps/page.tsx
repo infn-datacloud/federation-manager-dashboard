55d483a4f51967fb148c41b5461fa86a26f49af0
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-12-02 19:56:51 +0100

    Remove comment

--- src/app/idps/page.tsx
+++ src/app/idps/page.tsx
@@ -15,37 +15,6 @@ export default async function Idps() {
 		return <Custom401 />;
 	}
 
-	/* const items = [
-		{
-			id: '1',
-			name: 'IAM Cloud 1',
-			description:
-				'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-			endpoint: 'https://iam.cloud.infn.it/',
-			protocol: 'openid',
-			audience: 'aud',
-			groups_claim: 'group',
-		},
-		{
-			id: '2',
-			name: 'IAM Cloud 2',
-			description: 'INFN-Cloud identity provider',
-			endpoint: 'https://2-iam.cloud.infn.it/',
-			protocol: 'openid',
-			audience: 'aud',
-			groups_claim: 'group',
-		},
-		{
-			id: '3',
-			name: 'IAM Cloud 3',
-			description: 'INFN-Cloud identity provider 2',
-			endpoint: 'https://3-iam.cloud.infn.it/',
-			protocol: 'openid',
-			audience: 'aud',
-			groups_claim: 'group',
-		},
-	]; */
-
 	return (
 		<>
 			<Header
