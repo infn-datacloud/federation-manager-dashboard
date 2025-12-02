b2cb6dc46a4fe3b4254fdc5ddacd8ce232dd705d
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-12-02 11:28:14 +0100

    Add support to BetterAuth v1.4 stateless sessions
    This commit removes the need of having an external database.
    Known issue: when the container is restarted, cookies must be
    cleaned on the browser, otherwise a server side crash occours.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,39 +1,33 @@
-import { betterAuth } from 'better-auth';
-import { genericOAuth } from 'better-auth/plugins';
-import { getMigrations } from 'better-auth/db';
-import Database from 'better-sqlite3';
-
-// create or open your SQLite DB
-const db = new Database('auth.sqlite');
+import { betterAuth } from "better-auth";
+import { genericOAuth } from "better-auth/plugins";
 
 export const auth = betterAuth({
-	database: db,
-	baseURL: process.env.FM_ENDPOINT_URL,
-	secret: process.env.FM_AUTH_SECRET,
-	plugins: [
-		genericOAuth({
-			config: [
-				{
-					providerId: process.env.FM_OIDC_PROVIDER_ID!,
-					discoveryUrl: `${process.env.FM_OIDC_URL}/.well-known/openid-configuration`,
-					clientId: process.env.FM_OIDC_CLIENT_ID!,
-					clientSecret: process.env.FM_OIDC_CLIENT_SECRET!,
-					scopes: process.env.FM_OIDC_SCOPES?.split(' '),
-				},
-			],
-		}),
-	],
-	session: {
-		expiresIn: 60 * 60, // 1 hour
-		updateAge: 60 * 5,
-	},
+  baseURL: process.env.FM_ENDPOINT_URL,
+  secret: process.env.FM_AUTH_SECRET,
+  plugins: [
+    genericOAuth({
+      config: [
+        {
+          providerId: process.env.FM_OIDC_PROVIDER_ID!,
+          discoveryUrl: `${process.env.FM_OIDC_URL}/.well-known/openid-configuration`,
+          clientId: process.env.FM_OIDC_CLIENT_ID!,
+          clientSecret: process.env.FM_OIDC_CLIENT_SECRET!,
+          scopes: process.env.FM_OIDC_SCOPES?.split(" "),
+        },
+      ],
+    }),
+  ],
+  session: {
+    expiresIn: 3600, // 1 hour
+    strategy: "jwt",
+    disableSessionRefresh: true,
+    cookieCache: {
+      enabled: true,
+      maxAge: 3600,
+    },
+  },
+  account: {
+    storeStateStrategy: "cookie",
+    storeAccountCookie: true, // Store account data after OAuth flow in a cookie (useful for database-less flows)
+  },
 });
-
-// run migrations
-async function ensureSchema() {
-	const { runMigrations } = await getMigrations(auth.options);
-	await runMigrations();
-}
-
-// On startup or before first use
-await ensureSchema();
