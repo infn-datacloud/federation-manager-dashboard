53162556a4f5fc48df535ccab3a82ae620f43a49
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2025-12-03 15:36:46 +0100

    Provide support for prettier formatter
    Add 'npm run format' command to format the entire
    codebase.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+      ".vscode/**",
+      ".zed/**",
+      ".git/**",
+      ".idea/**",
+      ".prettierignore",
+      ".prettierrc.json",
+    ],
+  },
 ];
 
 export default eslintConfig;
